6208af3962361dbb410bb19398fa16bb1b9d5e51
humanbrainengineer <apacheindex@yahoo.com>
2015-12-26 17:53:55 +0800

    Create BCM4312.sh

---
+++ BCM4312.sh
@@ -0,0 +1,21 @@
+# driver
+# bcm4312 Linux driver
+
+# download tools.
+
+ su -
+ dnf -y install b43-fwcutter wget
+
+# download the bcm4312 linux driver.
+
+wget http://downloads.openwrt.org/sources/broadcom-wl-4.150.10.5.tar.bz2
+
+#tar the firmware to the  /lib/firmware.
+tar -xjvf broadcom-wl-4.150.10.5.tar.bz2
+cd broadcom-wl-4.150.10.5/driver
+b43-fwcutter -w /lib/firmware/ wl_apsta_mimo.o
+
+#reload the 43 kernel module.
+
+modprobe -r b43
+modprobe b43
